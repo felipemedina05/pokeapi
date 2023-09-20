15da81f23a63539c2be7de80b0b2691581acd67f
ANDRES FELIPE MEDINA MUÑOZ <107763296+felipemedina05@users.noreply.github.com>
2023-09-19 22:14:46 -0500

    conexion a poke api

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from "react";
+
 import './App.css'
+import Card from "./componets/card/Card";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [pokemon, setPokemon] = useState("charmander");
+  const [data, setData] = useState(null);
+
+
+  useEffect(()=>{
+    const getData = async ()=>{
+        const res = await fetch (`https://pokeapi.co/api/v2/pokemon/${pokemon}/`)
+        const datos = await res.json()
+        console.log (datos)
+        setData (datos)
+    }
+  },[])
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+   {data && <Card   data={data}/>}
     </>
   )
 }
